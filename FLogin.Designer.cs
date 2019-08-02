591233f9e113506354ddb090c827d8225f616e54
Thiago Jorge <thiagojorge.fatec@gmail.com>
2019-08-02 14:25:08 -0300

    Tela de login concluída!

---
+++ FLogin.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SGC
+{
+    partial class FLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(167, 378);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Entrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 110);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuário:";
+            // 
+            // txtUser
+            // 
+            this.txtUser.ForeColor = System.Drawing.Color.Gray;
+            this.txtUser.Location = new System.Drawing.Point(119, 110);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(265, 28);
+            this.txtUser.TabIndex = 2;
+            this.txtUser.Text = "Usuário";
+            this.txtUser.Enter += new System.EventHandler(this.TxtUser_Enter);
+            this.txtUser.Leave += new System.EventHandler(this.TxtUser_Leave);
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.ForeColor = System.Drawing.Color.Gray;
+            this.txtSenha.Location = new System.Drawing.Point(119, 186);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(265, 28);
+            this.txtSenha.TabIndex = 4;
+            this.txtSenha.Text = "Senha";
+            this.txtSenha.Enter += new System.EventHandler(this.TxtSenha_Enter);
+            this.txtSenha.Leave += new System.EventHandler(this.TxtSenha_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 189);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha:";
+            // 
+            // FLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 519);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ FLogin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGC
+{
+    public partial class FLogin : Form
+    {
+        public FLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtUser_Enter(object sender, EventArgs e)
+        {
+            txtUser.Text = "";
+        }
+
+        private void TxtUser_Leave(object sender, EventArgs e)
+        {
+            if (txtUser.Text == "")
+            {
+                txtUser.Text = "Informe o usúario";
+                txtUser.ForeColor = Color.Gray;
+            }
+                
+        }
+
+        private void TxtSenha_Enter(object sender, EventArgs e)
+        {
+            txtSenha.Text = "";
+
+        }
+
+        private void TxtSenha_Leave(object sender, EventArgs e)
+        {
+            if(txtSenha.Text == "")
+            {
+                txtSenha.Text = "Informe a senha";
+                txtSenha.ForeColor = Color.Gray;
+            }
+        }
+    }
+}
+
